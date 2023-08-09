8ef93edb4abe8dd9e17b16679b958e86dca328f0
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-09 18:30:27 -0500

    add comments to index.js and shape.tests

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+// Imports of required modules/packages
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generate_svg = require("./lib/shapes")
-
+// Function for validating input is not blank
 function validate_input(input){
     if(input == ""){
         return console.log("Cannot be Blank")
     }
     return true
 };
+// Function to validate input is 3 chacters long
 function validate_input_length(input){
     if(input.length != 3){
         return console.log(" Must be 3 Characters Long")
     }
     return true
 };
-
+// SVG parameters that will be set by command line
 const parameters =[
     {
         type: "list",
@@ -41,17 +43,18 @@ const parameters =[
         validate: validate_input_length
     }
 ];
-
+// Function to write the SVG File
 function write_svg(file_name, data){
     fs.writeFile(`./example/${file_name}`, data, (err) =>{
        err ? console.log(err) : console.log("Generated logo.svg")
     })
 }
-
+// Function to ask questions to build parameters
 function questions(){
     inquirer.prompt(parameters).then(data => {
         console.log(data);
         write_svg(`${data.shape}_logo.svg`,generate_svg(data))
     })
 }
+// Calls questions function to run program
 questions()
